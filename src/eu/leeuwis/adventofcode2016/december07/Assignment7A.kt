7cce56def74c4ab93d367f4ed46a4cc9b3e63581
Laurens Leeuwis <lleeuwis@summercamp.local>
2016-12-09 23:07:25 +0100

    add comment

--- src/eu/leeuwis/adventofcode2016/december07/Assignment7A.kt
+++ src/eu/leeuwis/adventofcode2016/december07/Assignment7A.kt
@@ -4,6 +4,7 @@ import java.io.File
 
 fun main(args: Array<String>) {
 
+    // input too large to declare as a val
     val result = File("src/eu/leeuwis/adventofcode2016/assignment7/input.txt").readLines().map {
                 IP7Address(it)
             }.filter {
