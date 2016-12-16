bbddb622383c0b88e22b1924c9dba113257607ce
Laurens Leeuwis <lleeuwis@summercamp.local>
2016-12-16 16:16:11 +0100

    moving

--- src/eu/leeuwis/adventofcode2016/december14/Assignment14.kt
+++ src/eu/leeuwis/adventofcode2016/december14/Assignment14.kt
@@ -1,3 +1,45 @@
 package eu.leeuwis.adventofcode2016.december14
 
+import java.security.MessageDigest
+
 val givenInput = "ahsbgdzn";
+val MD5_DIGEST = MessageDigest.getInstance("MD5")
+val HEX_CHARS = "0123456789abcdef".toCharArray()
+
+fun Byte.toHex(): List<Char> {
+    val i = this.toInt()
+    val char2 = HEX_CHARS[i and 0x0f]
+    val char1 = HEX_CHARS[i shr 4 and 0x0f]
+    return listOf(char1, char2)
+}
+
+fun calculateHash(input: String):CharArray {
+    return MD5_DIGEST.digest(input.toByteArray()).flatMap { it.toHex() }.toCharArray()
+}
+
+fun containsFiveOf(md5hash: CharArray, char: Char): Boolean {
+    md5hash.sliding(5).forEach{
+        if (it.sliding(2).map { it[0] == it[1] }.reduce { a, b -> a && b }) {
+            if (it[0] == char){
+                return true
+            }
+        }
+    }
+    return false
+}
+
+fun isTriple(md5hash: CharArray): Boolean {
+    return findTripleChar(md5hash) != null
+}
+
+fun findTripleChar(md5hash: CharArray): Char? {
+    md5hash.sliding(3).forEach{if(it.sliding(2).map{it[0] == it[1]}.reduce{a,b -> a && b}){
+        return it[0]
+    }}
+
+    return null
+}
+
+fun CharArray.sliding(size: Int): List<CharArray> {
+    return dropLast(size - 1).mapIndexed { index, boolean -> copyOfRange(index, index + size) }
+}
--- src/eu/leeuwis/adventofcode2016/december14/Assignment14B.kt
+++ src/eu/leeuwis/adventofcode2016/december14/Assignment14B.kt
@@ -1,27 +1,5 @@
 package eu.leeuwis.adventofcode2016.december14
 
-import java.security.MessageDigest
-
-val MD5_DIGEST = MessageDigest.getInstance("MD5")
-val HEX_CHARS = "0123456789abcdef".toCharArray()
-
-fun Byte.toHex(): List<Char> {
-    val i = this.toInt()
-    val char2 = HEX_CHARS[i and 0x0f]
-    val char1 = HEX_CHARS[i shr 4 and 0x0f]
-    return listOf(char1, char2)
-}
-
-
-/*
-For example, if the pre-arranged salt is abc:
-
-The first index which produces a triple is 18, because the MD5 hash of abc18 contains ...cc38887a5.... However, index 18 does not count as a key for your one-time pad, because none of the next thousand hashes (index 19 through index 1018) contain 88888.
-The next index which produces a triple is 39; the hash of abc39 contains eee. It is also the first key: one of the next thousand hashes (the one at index 816) contains eeeee.
-None of the next six triples are keys, but the one after that, at index 92, is: it contains 999 and index 200 contains 99999.
-Eventually, index 22728 meets all of the criteria to generate the 64th key.
- */
-
 
 fun main(args: Array<String>) {
 
@@ -32,16 +10,16 @@ fun main(args: Array<String>) {
         iteration++
 
         val input = givenInput + iteration
-        val md5hash = MD5_DIGEST.digest(input.toByteArray()).flatMap { it.toHex() }.toCharArray()
+        val md5hash = calculateHash(input)
 
         if (isTriple(md5hash)) {
             val char = findTripleChar(md5hash)!!
 
             for(i in iteration+1..iteration+1000){
                 val input2 = givenInput + i
-                val md5hash2 = MD5_DIGEST.digest(input2.toByteArray()).flatMap { it.toHex() }.toCharArray()
+                val md5hash2 = calculateHash(input)
 
-                if (containsFive(md5hash2, char)){
+                if (containsFiveOf(md5hash2, char)){
                     foundValues++
 
                     if (foundValues == 64){
@@ -53,30 +31,4 @@ fun main(args: Array<String>) {
     }
 }
 
-fun containsFive(md5hash: CharArray, char: Char): Boolean {
-    md5hash.sliding(5).forEach{
-        if (it.sliding(2).map { it[0] == it[1] }.reduce { a, b -> a && b }) {
-            if (it[0] == char){
-                return true
-            }
-        }
-    }
-    return false
-}
 
-
-fun isTriple(md5hash: CharArray): Boolean {
-    return findTripleChar(md5hash) != null
-}
-
-fun findTripleChar(md5hash: CharArray): Char? {
-    md5hash.sliding(3).forEach{if(it.sliding(2).map{it[0] == it[1]}.reduce{a,b -> a && b}){
-        return it[0]
-    }}
-
-    return null
-}
-
-fun CharArray.sliding(size: Int): List<CharArray> {
-    return dropLast(size - 1).mapIndexed { index, boolean -> copyOfRange(index, index + size) }
-}
