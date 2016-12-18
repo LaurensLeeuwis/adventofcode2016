baad9a49f04af91b1e6ee7ba51f566ccd627b780
Laurens Leeuwis <lleeuwis@summercamp.local>
2016-12-18 01:15:43 +0100

    Assignment 11B

--- src/eu/leeuwis/adventofcode2016/december11/Assignment11B.kt
+++ src/eu/leeuwis/adventofcode2016/december11/Assignment11B.kt
@@ -3,7 +3,7 @@ package eu.leeuwis.adventofcode2016.december11
 fun main(args: Array<String>) {
 
     val initialState = listOf(
-            setOf(Elevator, Microchip("promethium"), Generator("promethium")),
+            setOf(Elevator, Microchip("promethium"), Generator("promethium"), Generator("elerium"), Microchip("elerium"), Generator("dilithium"), Microchip("dilithium")),
             setOf(Generator("cobalt"), Generator("curium"), Generator("ruthenium"), Generator("plutonium")),
             setOf(Microchip("cobalt"), Microchip("curium"), Microchip("ruthenium"), Microchip("plutonium")),
             setOf())
